312c8498a0cde4faf9cb0b8f654ac037b953fda8
PanGan21 <ganelis.pan@gmail.com>
2025-04-14 14:11:13 +0300

    implement forward function

--- src/position_wise_feed_forward.py
+++ src/position_wise_feed_forward.py
@@ -7,3 +7,6 @@ class PositionWiseFeedForward(nn.Module):
         self.fc1 = nn.Linear(d_model, d_ff)
         self.fc2 = nn.Linear(d_ff, d_model)
         self.relu = nn.ReLU
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
