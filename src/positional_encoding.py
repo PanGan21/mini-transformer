61b97a6fdc7771915322197a9a1457d25968ab00
PanGan21 <ganelis.pan@gmail.com>
2025-04-16 10:23:19 +0300

    implement positional encoding class

---
+++ src/positional_encoding.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import math
+
+
+# Positional Encoding is used to inject the position information of each token in the input sequence.
+# It uses sine and cosine functions of different frequencies to generate the positional encoding.
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(
+            0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
